95b53bfef0b329c8fb72e1ea60faede52c1be2bf
ogee <ermmaculate97@gmail.com>
2026-02-15 13:59:24 +0100

    multiplication table assignment

--- Tasks.py
+++ Tasks.py
@@ -8,38 +8,63 @@
 """
 
 # solution
+# # 
+# grade = float(input("What is your grade score? "))
 
-grade = float(input("What is your grade score? "))
-
-if 3.5 <= grade <= 4.0:
-    print("First Class Honours")
-elif 3.0 <= grade < 3.49:  
-    print("Second Class Honours (Upper Division)")
-elif 2.0 <= grade < 2.99:   
-    print("Second Class Honours (Lower Division)")
-elif 1.0 <= grade < 2.0:
-    print("Third Class Honours")
-else:
-   print("Invalid Grade") 
+# if 3.5 <= grade <= 4.0:
+#     print("First Class Honours")
+# elif 3.0 <= grade < 3.49:  
+#     print("Second Class Honours (Upper Division)")
+# elif 2.0 <= grade < 2.99:   
+#     print("Second Class Honours (Lower Division)")
+# elif 1.0 <= grade < 2.0:
+#     print("Third Class Honours")
+# else:
+#    print("Invalid Grade") 
 
 
 
-"""
-Write a Python program that iterates through integers from 1 to 100.
-For each multiple of three, print "Fizz" instead of the number; 
-for each multiple of five, print "Buzz".
-For numbers that are multiples of both three and five, print "FizzBuzz".
- """
+# """
+# Write a Python program that iterates through integers from 1 to 100.
+# For each multiple of three, print "Fizz" instead of the number; 
+# for each multiple of five, print "Buzz".
+# For numbers that are multiples of both three and five, print "FizzBuzz".
+#  """
 
-integers = range(1,101)
+# integers = range(1,101)
 
-for i in integers:
-    if i % 3 ==0:
-        print("Fizz")
-    if i % 5 ==0:
-        print("Buzz")
-    if i % 3 ==0 and i % 5 ==0:
-        print("FizzBuzz")
-    else: 
-        print(i)
+# for i in integers:
+#     if i % 3 ==0:
+#         print("Fizz")
+#     if i % 5 ==0:
+#         print("Buzz")
+#     if i % 3 ==0 and i % 5 ==0:
+#         print("FizzBuzz")
+#     else: 
+#         print(i)
+
+
+
+# task 3 assignment
+
+# write 12 x timestable using the 2 methods
+
+
+numbers = range(1,13)
+
+for number in numbers:
+    value = 12 * number
+    print(f"12 x {number} = {value}")
+
+# method 2 
+
+numbers = range(1,101)
+
+for number in numbers:
+     value = int(number//12 )
+     if number % 12 ==0:
+            print(f"{number}/12 = {value}")
+
+
+
 
